a41547fdc728417587d1b5b7ad77f696fd2f2ad1
yuan-hongliang <2050364415@qq.com>
2022-04-24 18:42:07 +0800

    test update

--- frame/init/engine.py
+++ frame/init/engine.py
@@ -4,5 +4,8 @@ from frame.init.init_server import init_server
 
 def init(config):
     _post_mapping,_get_mapping=init_mapping(config)
+    print("包文件扫描成功")
     init_filter_dict(config,_get_mapping,_post_mapping)
+    print("过滤器加载成功")
     init_server(config)
+    print("请求队列初始化成功")
